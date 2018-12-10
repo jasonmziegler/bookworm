da82084946a283d8757429fda6c66a0b2ea30acd
jasonmziegler <jason@insightfulautomation.com>
2018-12-09 22:57:54 -0600

    add page restriction middleware

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var app = express();
 
+//mongodb connection
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+//mongo error
+db.on('error', console.error.bind(console, 'connection error'));
 
 // use sessions for tracking logins
 app.use(session({
   secret: 'WARCAMPAIGN',
   resave: true,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection:db
+  })
 }));
 
 // make user ID available in templates
@@ -17,12 +26,6 @@ app.use(function (req, res, next) {
   res.locals.currentUser = req.session.userId;
   next();
 });
-
-//mongodb connection
-mongoose.connect("mongodb://localhost:27017/bookworm");
-var db = mongoose.connection;
-//mongo error
-db.on('error', console.error.bind(console, 'connection error'));
 
 // parse incoming requests
 app.use(bodyParser.json());
